d2118f6b4090377a901ffec5feb43260533cb798
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2026-01-16 16:27:51 +0000

    handle double interupts on new stack

--- src/main.rs
+++ src/main.rs
@@ -29,9 +29,11 @@ pub extern "C" fn _start() -> ! {
 
     rusty_os::init();
 
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     #[cfg(test)]
     test_main();
