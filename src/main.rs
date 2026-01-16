5e5935d9b70c30c34d02fa51b8a862b8bc3e160d
Ben Walker <ben.walker@siemens.com>
2026-01-16 11:55:49 +0000

    trivial tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#![reexport_test_harness_main = "test_main"]
+
 use core::panic::PanicInfo;
 
 mod vga_buffer;
@@ -16,5 +21,24 @@ pub extern "C" fn _start() -> !{
     
     println!("Hello World{}", "!");
 
+    #[cfg(test)]
+    test_main();
+
     loop {}
+}
+
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("Trivial assertion... ");
+    assert_eq!(1,1);
+    println!("[ok]");
 }
