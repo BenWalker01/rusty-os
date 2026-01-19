dec0fd1d8c0480bb6dd66c51457ce854af1ea034
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2026-01-19 10:16:06 +0000

    start implementation for hardware interrupts

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,6 @@ pub extern "C" fn _start() -> ! {
 
     rusty_os::init();
 
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
 
     #[cfg(test)]
     test_main();
