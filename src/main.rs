c3ef103afd390131d9faf53c253f8ffc711d7c37
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2026-01-20 10:09:14 +0000

    translate addresses

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 use bootloader::{BootInfo, entry_point};
 use core::panic::PanicInfo;
-use rusty_os::println;
+use rusty_os::{memory::translate_addr, println};
 use x86_64::structures::paging::PageTable;
 
 #[cfg(not(test))]
@@ -25,30 +25,28 @@ fn panic(info: &PanicInfo) -> ! {
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use rusty_os::memory::active_level_4_table;
     use x86_64::VirtAddr;
 
     println!("Hello World{}", "!");
     rusty_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {}: {:?}", i, entry);
+    let addresses = [
+        // vga buffer page
+        0xb8000,
+        // some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virtual mem address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!("  L3 Entry {}: {:?}", i, entry);
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{:?} -> {:?}", virt, phys);
     }
 
     #[cfg(test)]
