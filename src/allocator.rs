6e9eb840b234eca0b0b4a1e6373db143f5949a8f
Ben Walker <146777065+BenWalker01@users.noreply.github.com>
2026-01-26 11:23:36 +0000

    fixed block allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,22 +1,24 @@
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
 use x86_64::{
     VirtAddr,
     structures::paging::{
         FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB, mapper::MapToError,
     },
 };
-use bump::BumpAllocator;
+// use bump::BumpAllocator;
+// use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedSizeBlockAllocator;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
 
 #[global_allocator]
-static ALLOCATOR: Locked<BumpAllocator> = Locked::new(BumpAllocator::new());
+static ALLOCATOR: Locked<FixedSizeBlockAllocator> = Locked::new(FixedSizeBlockAllocator::new());
 
 pub fn init_heap(
     mapper: &mut impl Mapper<Size4KiB>,
@@ -58,7 +60,7 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 pub struct Locked<A> {
-    inner: spin::Mutex<A>
+    inner: spin::Mutex<A>,
 }
 
 impl<A> Locked<A> {
